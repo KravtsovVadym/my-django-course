d83e8442263136730148ebbf32d54afa4a73c5b0
Vadym <vadymkravtcsov@gmail.com>
2026-01-22 18:38:44 +0200

    re: create a Note app and  migrations have been made

---
+++ Django-Basics/django_project/notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models # type: ignore
+
+class Note(models.Model):
+    title = models.CharField('Title', max_length=100)
+    body = models.TextField('Note')
+    create_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes'
