416bc808556ca2ff8fc4e7bf6ddc4f3fe0a35131
Vadym <vadymkravtcsov@gmail.com>
2026-02-01 19:52:34 +0200

    feat: add (сustom User and UserManager) models

---
+++ Django-Authen-Authz/django_project/accounts/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Field (email) cannot be empty')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField('first name', max_length=50)
+    last_name = models.CharField('last name', max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    # Retur True if the user is allowed to have access the admin site.
+    @property
+    def is_staff(self):
+        return self.is_superuser
