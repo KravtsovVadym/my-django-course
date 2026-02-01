d521f99cb001dd39994deccd93c9473c9f2bb26c
Vadym <vadymkravtcsov@gmail.com>
2026-02-01 19:14:32 +0200

    feat: add (Tag) models with unique 'name' constraints

--- Django-Authen-Authz/django_project/weblog/models.py
+++ Django-Authen-Authz/django_project/weblog/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from django.db.models.functions import Lower
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Tag', max_length=10, unique=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                Lower("name"),  # Overcomes uniqueness through lower()
+                name="unique_lower_name_tag")  # Technical identifier
+        ]
+        ordering  = ['name']  # Sorted (name Tag) alphabetically
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
