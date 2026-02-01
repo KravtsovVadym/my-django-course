ef06202e7e985dfc5880a19626c894aea7b10556
Vadym <vadymkravtcsov@gmail.com>
2026-02-01 19:54:52 +0200

    feat: add (CreateUser and ChangeUser) forms

---
+++ Django-Authen-Authz/django_project/accounts/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from accounts.models import User
+from django.core.exceptions import ValidationError
+
+class CreateUserForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+        )
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput
+        )
+
+    class Meta:
+        models = User
+        fields = ['email']
+
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+        return password2
+
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+class ChangeUserForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
